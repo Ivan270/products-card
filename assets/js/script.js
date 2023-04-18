537f986411f91ca5f4fd2d91a7e14f3be9adce9a
Ivan Verdugo <ivan270@gmail.com>
2023-04-18 19:48:03 -0400

    project initialized

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+function Producto(marca, modelo, precio, img) {
+	this.brand = marca;
+	this.model = modelo;
+	this.price = precio;
+	this.img = img;
+}
+let teclado1 = new Producto(
+	'Logitech',
+	'G915 TKL',
+	174900,
+	'https://media.solotodo.com/media/products/1185583_picture_1592642933.png'
+);
+
+let teclado2 = new Producto(
+	'Steelseries',
+	'Apex Pro Mini Wireless',
+	283290,
+	'https://media.solotodo.com/media/products/1623783_picture_1657640378.jpg'
+);
