3af1ad184262fd560fa842136612c5ae14d76b16
Ivan Verdugo <ivan270@gmail.com>
2023-04-18 21:01:13 -0400

    improved logic to display each product

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,35 @@ let teclado2 = new Producto(
 	283290,
 	'https://media.solotodo.com/media/products/1623783_picture_1657640378.jpg'
 );
+
+let displayModelo = document.getElementById('productModel');
+let displayMarca = document.getElementById('productBrand');
+let displayPrecio = document.getElementById('productPrice');
+let displayImage = document.getElementById('productImage');
+let btnAlternativa = document.getElementById('btnAlternativa');
+
+let mostrarProducto = (producto) => {
+	displayModelo.innerHTML = producto.model;
+	displayMarca.innerHTML = producto.brand;
+	displayPrecio.innerHTML = producto.price.toLocaleString('es-CL', {
+		style: 'currency',
+		currency: 'CLP',
+	});
+	displayImage.setAttribute('src', producto.img);
+};
+
+let productos = [];
+productos.push(teclado1);
+productos.push(teclado2);
+
+// Recorre array de productos, con cada click muestra un producto distinto dependiendo de su ubicacion en el array
+let index = 0;
+btnAlternativa.addEventListener('click', () => {
+	if (index < productos.length) {
+		mostrarProducto(productos[index]);
+	} else {
+		index = 0;
+		mostrarProducto(productos[index]);
+	}
+	index++;
+});
